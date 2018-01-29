a9bcd4fc6184474f689f8ed3bd3fafe26dc70850
Ross Hill <rosshill88@gmail.com>
2018-01-29 11:50:44 +0000

    Created basic layout, constructor in MainActivity

--- app/src/main/java/com/example/rosshill/fruitmachine/MainActivity.java
+++ app/src/main/java/com/example/rosshill/fruitmachine/MainActivity.java
@@ -9,44 +9,13 @@ import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
-    public class Game {
+    Game new_game;
+    TextView textView;
 
-        FruitMachine fruitMachine;
-        Player player;
-
-        public Game(FruitMachine fruitMachineFull, Player player) {
-            this.fruitMachine = fruitMachineFull;
-            this.player = player;
-        }
-
-        public void startGame() {
-            Scanner reader = new Scanner(System.in);
-            System.out.println("Welcome " + player.getName() + ", your balance is £" + player.getWallet());
-            while (player.getWallet() > 0) {
-                System.out.println("Type SPIN to play or QUIT to end game");
-                System.out.println("Your current balance is: " + player.getWallet());
-                String selection;
-                selection = reader.next();
-                selection = selection.toUpperCase();
-                if (selection.equals("SPIN")) {
-                    player.spendMoney(0.5);
-                    int result = fruitMachine.spin();
-                    if (result > 0) {
-                        System.out.println("Congrats! You won £");
-                        player.wallet += result;
-                    } else {
-                        System.out.println("Sorry, you lost");
-                    }
-
-                } else if (selection.equals("QUIT")) {
-                    System.out.println("Thanks for playing! Your balance is £" + player.getWallet());
-                    break;
-                } else {
-                    System.out.println("I don't recognise that command.");
-                }
-            }
-
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
     }
 }
