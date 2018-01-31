03a7542554e26cb3b2e87a65bf92c6ffd202e062
Ross Hill <rosshill88@gmail.com>
2018-01-31 15:30:32 +0000

    Added transparency to text fields, text alignment and tweaks to size

--- app/src/main/java/com/example/rosshill/fruitmachine/OpenScreen.java
+++ app/src/main/java/com/example/rosshill/fruitmachine/OpenScreen.java
@@ -2,12 +2,26 @@ package com.example.rosshill.fruitmachine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class OpenScreen extends AppCompatActivity {
+
+    Button classic_button;
+    Button codeclan_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_screen);
+    }
+
+    public void onClassicButtonClick(View classic_button) {
+
+    }
+
+    public void onCodeCLanButtonClick(View codeclan_button) {
+
     }
 }
